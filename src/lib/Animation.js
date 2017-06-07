e69cc1b1d1bc379afff7cfc189d485a13392d087
Daniel Gómez <dani91.gomez@gmail.com>
2017-06-07 20:55:00 -0300

    Added merge from react-animations

--- src/lib/Animation.js
+++ src/lib/Animation.js
@@ -2,9 +2,12 @@ import styled, { keyframes } from 'styled-components';
 
 import * as animations from 'react-animations';
 
-const frames = (name) => keyframes`
-   ${animations[name]}
-`;
+const mergeAnimations = (animationArray) => animationArray.reduce((prev, next, index) => animations.merge(index? prev : animations[prev], animations[next]));
+
+const frames = (name) => {
+    const selectedAnimations = name.split(" ");
+    return keyframes`${selectedAnimations.length > 1? mergeAnimations(selectedAnimations) : animations[name]}`;
+};
 
 const Animation = styled.div`
     animation: ${({name}) => frames(name)} ${({duration}) => duration? duration : "1s"} ${({timing}) => timing? timing : "ease"};
